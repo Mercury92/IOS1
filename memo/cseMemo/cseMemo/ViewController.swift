d4d99ef44229a33dd65e32d252c06168a49d7bd3
choishuning <cse555@naver.com>
2020-06-01 19:25:54 +0900

    memo

---
+++ memo/cseMemo/cseMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cseMemo
+//
+//  Created by choisueun on 01/06/2020.
+//  Copyright © 2020 최수은. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
