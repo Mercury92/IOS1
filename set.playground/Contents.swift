0daf909261e7e8549ebc1fab1847c30d156ba2a9
choishuning <cse555@naver.com>
2020-06-02 20:49:42 +0900

    set

---
+++ set.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+var str = "Hello, playground"
+
+/// Set
+
+// 빈 Int Set 생성
+var integerSet: Set<Int> = Set<Int>()
+integerSet.insert(1)
+integerSet.insert(100)
+integerSet.insert(99)
+integerSet.insert(99)
+integerSet.insert(99)
+
+print(integerSet) // [100, 99, 1]
+print(integerSet.contains(1)) // true
+print(integerSet.contains(2)) // false
+
+integerSet.remove(100)
+integerSet.removeFirst()
+
+print(integerSet.count) // 1
+
+// Set는 집합 연산에 꽤 유용합니다
+let setA: Set<Int> = [1, 2, 3, 4, 5]
+let setB: Set<Int> = [3, 4, 5, 6, 7]
+
+// 합집합
+let union: Set<Int> = setA.union(setB)
+print(union) // [2, 4, 5, 6, 7, 3, 1]
+
+// 합집합 오름차순 정렬
+let sortedUnion: [Int] = union.sorted()
+print(sortedUnion) // [1, 2, 3, 4, 5, 6, 7]
+
+// 교집합
+let intersection: Set<Int> = setA.intersection(setB)
+print(intersection) // [5, 3, 4]
+
+// 차집합
+let subtracting: Set<Int> = setA.subtracting(setB)
+print(subtracting) // [2, 1]
