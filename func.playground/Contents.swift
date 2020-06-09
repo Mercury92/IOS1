a57fce20241dc37ae61d8b26f768c8c9008c4750
choishuning <cse555@naver.com>
2020-06-09 12:14:37 +0900

    Update Contents.swift

--- func.playground/Contents.swift
+++ func.playground/Contents.swift
@@ -1,61 +1,54 @@
 import UIKit
 
-var str = "Hello, playground"
 
-
-// 예)
-// sum이라는 이름을 가지고
-// a와 b라는 Int 타입의 매개변수를 가지며
-// Int 타입의 값을 반환하는 함수
-func sum(a: Int, b: Int) -> Int {
-    return a + b
+// 함수 선언의 기본형태
+// sum이라는 이름을 가지는 함수로
+// data1과 data2라는 Int타입의 매개변수를 가지고
+// Int타입의 값으로 반환
+func sum(data1: Int, data2: Int) -> Int {
+    return data1 + data2
 }
 
-// 반환 값이 없는 함수
+// 함수 호출
+// 결과값 3
+sum(data1: 1, data2: 2)
+
+
+// 반환값이 없는 함수
+// 반환값이 없는 함수의 경우 변환 타입(Void) 생략가능
 func printMyName(name: String) -> Void {
     print(name)
 }
 
-// 반환 값이 없는 경우, 반환 타입(Void)을 생략해 줄 수 있습니다
-func printYourName(name: String) {
-    print(name)
-}
+// 함수 호출
+// 결과 값 choisueun
+printMyName(name: "choisueun")
+
 
 // 매개변수가 없는 함수
 func maximumIntegerValue() -> Int {
     return Int.max
 }
 
-//매개변수와 변환값이 없는 함수
-// 함수 구현이 짧은 경우 가독성을 해치지 않는 범위에서 줄바꿈을 하지 않고 한 줄에 표현해도 무관
-func hello() -> Void { print("hello") }
-
-// 반환값이 없는 경우에는 반환 타입(Void)을 생략해 줄 수 있습니다
-func bye() { print("bye") }
-
-
-
-sum(a: 3, b: 5) // 8
-
-printMyName(name: "yagom") // yagom
-
-printYourName(name: "hana") // hana
-
+// 함수 호출
+// 결과 값 Int의 최대값출력
 maximumIntegerValue() // Int의 최댓값
 
-hello() // hello
 
-bye() // bye
-
-
-
-
-sum(a: 1, b: 2)
-print(sum(a: 1, b: 2))
-print(printMyName(name: "cse"))
-print(printYourName(name: "sueun"))
-// multiply
+// 매개변수와 반환값이 없는 함수
+// 반환 값이 없는 경우, 반환 타입(Void)을 생략해 줄 수 있습니다
+func bye() {
+    return
+}
 
 
+// 함수 구현이 짧은 경우
+// 가독성을 해치지 않는 범위에서
+// 줄바꿈을 하지 않고 한 줄에 표현가능
+func hello() -> Void { print("hello") }
+func byebye() { print("bye") }
 
-
+// 함수 호출
+// 결과 값  hello, bye
+hello()
+byebye()
